2412e9a901202ded6b92e5123d3bdb4a43379e12
Andrii Novoselskyi <andrii@qooco.com.ua>
2018-06-01 17:40:30 +0300

    Added iOS Cocoa Touch framework

---
+++ RBBAnimation_iOS/RBBAnimation_iOS.h
@@ -0,0 +1,23 @@
+//
+//  RBBAnimation_iOS.h
+//  RBBAnimation_iOS
+//
+//  Created by Andrii Novoselskyi on 6/1/18.
+//  Copyright © 2018 Robert Böhnke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RBBCustomAnimation.h"
+#import "RBBTweenAnimation.h"
+#import "RBBSpringAnimation.h"
+
+//! Project version number for RBBAnimation_iOS.
+FOUNDATION_EXPORT double RBBAnimation_iOSVersionNumber;
+
+//! Project version string for RBBAnimation_iOS.
+FOUNDATION_EXPORT const unsigned char RBBAnimation_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RBBAnimation_iOS/PublicHeader.h>
+
+
